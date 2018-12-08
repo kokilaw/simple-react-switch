a984d710b99cdc3c2579693afb6246a6ce90c994
Kokila Widyaratna <kokilapc@gmail.com>
2018-12-09 00:23:53 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -5,36 +5,4 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import DemoPage from './demo'
 
-// class App extends Component {
-//     constructor() {
-//         super()
-//         this.state = { switchState: false }
-//         this.handleChange = this.handleChange.bind(this)
-//     }
-
-//     handleChange(isChecked, key, event) {
-//         this.setState({ switchState: isChecked })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <div>
-//                     <HorizontalSwitch
-//                         onStateChange={this.handleChange}
-//                         id={'123456'}
-//                         onColor={'#48EA8B'}
-//                         offColor={'#FF4651'}
-//                         disabledColor={'#D3D3D3'}
-//                         foregroundColor={'#fff'}
-//                     />
-//                 </div>
-//                 <div style={{ textAlign: 'center' }}>
-//                     {this.state.switchState ? 'ON' : 'OFF'}
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
 ReactDOM.render(<DemoPage />, document.getElementById('root'))
